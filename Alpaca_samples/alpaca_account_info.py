54030299d59bb8672698a1fc0dc5ab54b694446d
jonathan <lndbearshark@gmail.com>
2021-06-15 12:51:03 -0600

    Updated alpaca api usage

---
+++ Alpaca_samples/alpaca_account_info.py
@@ -0,0 +1,9 @@
+import secrets
+import alpaca_trade_api as tradeapi
+
+key = secrets.paper_key
+secret = secrets.paper_secret
+base = secrets.paper_base
+api = tradeapi.REST(key, secret, base, 'v2')
+account = api.get_account()
+print(account)
--- Alpaca_samples/alpaca_endpoints.py
+++ Alpaca_samples/alpaca_endpoints.py
@@ -1,5 +1,6 @@
 import secrets
 import alpaca_trade_api as tradeapi
+from math import floor
 
 key = secrets.paper_key
 secret = secrets.paper_secret
@@ -7,6 +8,7 @@ base = secrets.paper_base
 api = tradeapi.REST(key, secret, base, 'v2')
 account = api.get_account()
 # print(account.status)
+# https://github.com/alpacahq/alpaca-trade-api-python/blob/master/alpaca_trade_api/rest.py
 
 
 # Get price of an asset
@@ -19,7 +21,14 @@ def get_asset_price(asset):
 def buy_or_sell(asset, dollars, amount, side, _type="market", time_in_force="day"):
     if side == "buy" or side == "sell":
         if dollars:
-            api.submit_order(symbol=asset, notional=amount, side=side, type=_type, time_in_force=time_in_force)
+            # Check if asset is fractionable
+            if api.get_asset(asset).fractionable:
+                api.submit_order(symbol=asset, notional=amount, side=side, type=_type, time_in_force=time_in_force)
+            else:
+                # grab leftover cash
+                shares = floor(amount / api.get_last_trade(asset).price)
+                print(f"Buying {shares} shares of {asset} because it is not fractional")
+                api.submit_order(symbol=asset, qty=shares, side=side, type=_type, time_in_force=time_in_force)
         else:
             api.submit_order(symbol=asset, qty=amount, side=side, type=_type, time_in_force=time_in_force)
     else:
@@ -29,14 +38,21 @@ def buy_or_sell(asset, dollars, amount, side, _type="market", time_in_force="day
 # get info on assets
 def get_info_on_assets():
     api.list_assets()
+    api.list_positions()
 
 
-sym = "AAPL"
-print(f"Getting price for {sym}")
-get_asset_price("AAPL")
-print(f"Buy order for 1 share of {sym}")
-buy_or_sell(asset=sym, dollars=False, amount=1, side='buy')
-print(f"Buy $100 of {sym}")
-buy_or_sell(asset=sym, dollars=True, amount=100, side='buy')
-print(f"list assets")
-get_info_on_assets()
+# sym = "QQQ"
+# print(api.get_asset(sym))
+# if api.get_asset(sym).fractionable == False:
+#     print("here")
+# buy_or_sell(asset=sym, dollars=True, amount=5000, side='buy')
+
+# sym = "AAPL"
+# print(f"Getting price for {sym}")
+# get_asset_price("AAPL")
+# print(f"Buy order for 1 share of {sym}")
+# buy_or_sell(asset=sym, dollars=False, amount=1, side='buy')
+# print(f"Buy $100 of {sym}")
+# buy_or_sell(asset=sym, dollars=True, amount=100, side='buy')
+# print(f"list assets")
+# get_info_on_assets()
