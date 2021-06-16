33475d42d3aedf01585f788788c1424b8e768e4c
jonathan <lndbearshark@gmail.com>
2021-06-15 18:07:50 -0600

    Added new app files

--- Alpaca_samples/alpaca_account_info.py
+++ Alpaca_samples/alpaca_account_info.py
@@ -6,4 +6,6 @@ secret = secrets.paper_secret
 base = secrets.paper_base
 api = tradeapi.REST(key, secret, base, 'v2')
 account = api.get_account()
+
+# print(f"Cash in account: ${round(float(account.cash), 2)}")
 print(account)
---
+++ Connector.py
@@ -0,0 +1,9 @@
+import secrets
+import alpaca_trade_api as tradeapi
+
+
+"""
+The purpose of this file is to create a common connection api that can connect to multiple brokers
+"""
+
+
